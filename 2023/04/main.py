dd34297b48f965429f2873ba6601fed3083c0adb
Cosmin Nicolaescu <cos@cosn.dev>
2024-01-31 16:34:08 -0800

    solve day4, part 2

--- 2023/04/main.py
+++ 2023/04/main.py
@@ -4,17 +4,24 @@ input = [i.strip() for i in open("input","r").readlines()]
 
 def solve():
     points = 0;
-    for line in input:
-        numbers = re.search(f"Card\s+(\d+):\s+([\d\s]+)\s+\|\s+([\d\s]+)", line)
+    copies = [1] * len(input)
+
+    for i, line in enumerate(input):
+        numbers = re.search(f"Card\s+\d+:\s+([\d\s]+)\s+\|\s+([\d\s]+)", line)
         
-        c_set = set(int(n) for n in numbers.group(2).split(' ') if len(n) > 0)
-        m_set = set(int(n) for n in numbers.group(3).split(' ') if len(n) > 0)
+        c_set = set(int(n) for n in numbers.group(1).split(' ') if len(n) > 0)
+        m_set = set(int(n) for n in numbers.group(2).split(' ') if len(n) > 0)
         
         matches = c_set.intersection(m_set)
         
         if len(matches) > 0:
             points += pow(2, len(matches)-1)
+
+            for _ in range(copies[i]):
+                for j in range(len(matches)):
+                    copies[i+j+1] += 1
     
     print(points)
+    print(sum(copies))
 
 solve()
