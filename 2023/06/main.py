46e75b565b817580ebff830be22c81ef3b32101f
Cosmin Nicolaescu <cos@cosn.dev>
2024-02-02 15:39:05 -0800

    solve day 6, part 2

--- 2023/06/main.py
+++ 2023/06/main.py
@@ -20,7 +20,7 @@ def analyze(record) -> int:
     return winners
 
 def solve():
-    times = list(map(int, input[0].split(":")[1].split()))
+    """ times = list(map(int, input[0].split(":")[1].split()))
     distances = list(map(int, input[1].split(":")[1].split()))
 
     records = list()
@@ -33,6 +33,11 @@ def solve():
         permutations.append(analyze(r))
 
     print(reduce(lambda x, y: x * y, permutations))
+    """
+
+    time = int("".join(input[0].split(":")[1].split()))
+    distance = int("".join(input[1].split(":")[1].split()))
+    print(analyze(Record(time, distance)))
 
 solve()
     
