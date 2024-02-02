d88d8bc811086ab3d155230ac1d8b0fa0e95c4fc
Cosmin Nicolaescu <cos@cosn.dev>
2024-02-02 15:21:12 -0800

    solve day 6, part 1

---
+++ 2023/06/main.py
@@ -0,0 +1,38 @@
+from functools import reduce
+
+input = [i.strip() for i in open('input', 'r').readlines()]
+
+class Record:
+    def __init__(self, time, distance):
+        self.time = time
+        self.distance = distance
+    
+    def __str__(self):
+        return f"Time: {self.time}, Distance: {self.distance}"  
+
+def analyze(record) -> int:
+    winners = 0
+
+    for i in range(1, record.time):
+        if i * (record.time - i) > record.distance:
+            winners += 1
+    
+    return winners
+
+def solve():
+    times = list(map(int, input[0].split(":")[1].split()))
+    distances = list(map(int, input[1].split(":")[1].split()))
+
+    records = list()
+
+    for i in range(0, len(times)):
+        records.append(Record(times[i], distances[i]))
+
+    permutations = list()
+    for r in records:
+        permutations.append(analyze(r))
+
+    print(reduce(lambda x, y: x * y, permutations))
+
+solve()
+    
